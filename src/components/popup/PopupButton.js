511554c08cdc263e02e47ad1f2dafd62365749cb
thind9xdev <149924467+thind9xdev@users.noreply.github.com>
2025-12-25 18:41:10 +0700

    fix: update react 19

--- src/components/popup/PopupButton.js
+++ src/components/popup/PopupButton.js
@@ -1,5 +1,5 @@
-import PropTypes from 'prop-types';
 import React from 'react';
+import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import ClickableComponent from '../ClickableComponent';
 import Popup from './Popup';
